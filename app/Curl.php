f5c2dbdd55143f3561dc020840c507dfcde5ed28
Roman Vycanek <roman@debian.utb.cz>
2020-07-20 12:39:46 -0500

    curl nmap test update

--- app/Curl.php
+++ app/Curl.php
@@ -21,34 +21,22 @@ class Curl extends Model
     public function runTest(String $adress, Int $testId)
     {
         //Start test
-        $process = new Process(['curl','-v', str_replace("https://", "", $adress)],$cwd = base_path() . '/app/Http/Controllers');
+        $process = new Process(['curl','-v', str_replace("https://", "", $adress)]);
         $process->setTimeout(0);
         try {
             $process->mustRun();
         
             //Format for DB save  
             $terminalResults = explode("\n", $process->getOutput());
-            $databaseForm = array();
-            for ($i = 0; $i < count($terminalResults); $i++) {
-            // if(str_contains($terminalResults[$i], 'Running client')){  
-                    for ($j = $i+1; $j < count($terminalResults); $j++) {
-                        if(strlen($terminalResults[$j])>8){
-                            array_push($databaseForm, rtrim($terminalResults[$j]));
-                        }
-                    }
-                    $i = $j;
-            //  }
-            }
-            //array_pop($databaseForm);
 
             //Save to DB 
-            foreach ($databaseForm as $line) {
+            foreach ($terminalResults as $line) {
                     $curl=new Curl;
                     $curl->test_id=$testId;
                     $curl->data=$line;
                     $curl->save();
             }
-            return [$process->getOutput()];
+            return [$terminalResults];
 
         } catch (ProcessFailedException $exception) {
             return [$exception->getMessage()];
--- app/Nmap.php
+++ app/Nmap.php
@@ -22,26 +22,14 @@ class Nmap extends Model
     public function runTest(String $adress, Int $testId)
     {
         //Start test
-        $process = new Process(['nmap', '-Ap1-65535',str_replace("https://", "", $adress)]);
+        $process = new Process(['nmap']);
         $process->setTimeout(0);
         try {
             $process->mustRun();
 
             //Format for DB save  
-            $terminalResults = explode("\n", $process->getOutput());
-            $databaseForm = array();
-            for ($i = 0; $i < count($terminalResults); $i++) {
-                if(str_contains($terminalResults[$i], 'Starting')){  
-                    for ($j = $i+1; $j < count($terminalResults); $j++) {
-                        if(strlen($terminalResults[$j])>8){
-                            array_push($databaseForm, rtrim($terminalResults[$j]));
-                        }
-                    }
-                    $i = $j;
-                }
-            }
-            //array_pop($databaseForm);
-
+            $databaseForm = explode("\n", $process->getOutput());
+            
             //Save to DB 
             foreach ($databaseForm as $line) {
                     $nmap=new Nmap;
