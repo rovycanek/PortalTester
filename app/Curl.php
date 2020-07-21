0f090d725914bf576b00a58adc80b322c142a4f6
Roman Vycanek <roman@ssltest.ptlab.utb.cz>
2020-07-21 22:28:29 +0000

    New Curl method

--- app/Curl.php
+++ app/Curl.php
@@ -20,34 +20,33 @@ class Curl extends Model
 
     public function runTest(String $adress, Int $testId)
     {
-        //Start test
         $process = new Process(['curl','-v' ,'--compressed', $adress]);
         $process->setTimeout(0);
         try {
             $process->mustRun();
-        
-            //Format for DB save  
+            //Format for DB save
             $terminalResults = explode("\n",  mb_convert_encoding($process->getErrorOutput() .$process->getOutput(), 'UTF-8','UTF-8'));
-     
             $maxlengh=array();
             foreach ($terminalResults as $line) {
-                foreach (str_split($line, 142) as $split) {
-                    array_push($maxlengh, $split);
+                while (mb_strlen( $line ) >0){
+                        array_push($maxlengh, mb_substr( $line, 0, 142, 'utf-8'));
+                        $line  = mb_substr( $line, 142, mb_strlen( $line ), 'utf-8' );
                 }
             }
 
-            //Save to DB 
+            //Save to DB
             foreach ($maxlengh as $line) {
                     $curl=new Curl;
                     $curl->test_id=$testId;
                     $curl->data=$line;
                     $curl->save();
             }
-            
+
             return $maxlengh;
         } catch (ProcessFailedException $exception) {
             return [$exception->getMessage()];
         }
+
     }
 
 }
